52a9fe22551b10c734ebf96c7bafb57a46d9a5d9
Its0mar <omarjadallah24@gmail.com>
2026-03-11 01:58:59 +0300

    initial project setup with Result pattern

---
+++ DeskForge.Api/Common/Results/ErrorType.cs
@@ -0,0 +1,12 @@
+namespace DeskForge.Api.Common.Results;
+
+public enum ErrorType
+{
+    Failure,
+    Unexpected,
+    Validation,
+    Conflict,
+    NotFound,
+    Unauthorized,
+    Forbidden,
+}
---
+++ DeskForge.Api/Common/Results/IResult.cs
@@ -0,0 +1,12 @@
+namespace DeskForge.Api.Common.Results;
+
+public interface IResult
+{
+    bool IsSuccess { get; }
+    List<Error>? Errors { get; }
+}
+
+public interface IResult<out TValue> : IResult
+{
+    TValue? Value { get; }
+}

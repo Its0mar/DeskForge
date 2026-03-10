a711ddeb7a250df14d3cd878f75a930c0d99a2c6
Its0mar <omarjadallah24@gmail.com>
2026-03-11 02:41:14 +0300

    Created ApiController.cs

---
+++ src/DeskForge.Api/Common/Controllers/ApiController.cs
@@ -0,0 +1,48 @@
+using DeskForge.Api.Common.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace DeskForge.Api.Common.Controllers;
+
+[ApiController]
+public class ApiController : ControllerBase
+{
+    protected ActionResult Problem(List<Error> errors)
+    {
+        if (errors.Count == 0)
+        {
+            return Problem();
+        }
+
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+
+        return Problem(errors[0]);
+    }
+    
+    
+    private ObjectResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
+        {
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
+            ErrorType.Forbidden => StatusCodes.Status403Forbidden,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return Problem(statusCode: statusCode, title: error.Description);
+    }
+
+    private ActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+        errors.ForEach(error => modelStateDictionary.AddModelError(error.Code, error.Description));
+        
+        return ValidationProblem(modelStateDictionary);
+    }
+}
